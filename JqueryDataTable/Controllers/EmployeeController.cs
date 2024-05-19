84255a8c2efe71642c83b50f4eb702e371c71d5a
aderine2015@gmail.com <aderine2015@gmail.com>
2024-05-19 17:59:38 +0800

    implement dataTable and add styling

--- JqueryDataTable/Controllers/EmployeeController.cs
+++ JqueryDataTable/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JqueryDataTable.Models;
 
 namespace JqueryDataTable.Controllers
 {
@@ -12,6 +13,15 @@ namespace JqueryDataTable.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetList()
+        {
+            using(DBModels db = new DBModels())
+            {
+                var empList = db.Employees.ToList<Employee>();
+                return Json(new {data = empList}, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
