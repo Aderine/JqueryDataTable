de67ecd316cda36b0a52c9cb4c8b20adcb95139d
aderine2015@gmail.com <aderine2015@gmail.com>
2024-05-21 07:13:42 +0800

    pagination

--- JqueryDataTable/Controllers/EmployeeController.cs
+++ JqueryDataTable/Controllers/EmployeeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using JqueryDataTable.Models;
+using System.Linq.Dynamic;
+
 
 namespace JqueryDataTable.Controllers
 {
@@ -15,12 +17,42 @@ namespace JqueryDataTable.Controllers
             return View();
         }
 
+        [HttpPost]
         public ActionResult GetList()
-        {
-            using(DBModels db = new DBModels())
+        { 
+            //server side params
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDir = Request["order[0][dir]"];
+
+            using (DBModels db = new DBModels())
             {
                 var empList = db.Employees.ToList<Employee>();
-                return Json(new {data = empList}, JsonRequestBehavior.AllowGet);
+
+                int totalRows = empList.Count;
+
+                //searching
+                if(!string.IsNullOrEmpty(searchValue))
+                {
+                    empList = empList.Where(x => x.Name.ToLower().Contains(searchValue.ToLower())||
+                    x.Position.ToLower().Contains(searchValue.ToLower()) ||
+                    x.Office.ToLower().Contains(searchValue.ToLower())||
+                    x.Age.ToString().Contains(searchValue.ToLower()) ||
+                    x.Salary.ToString().Contains(searchValue.ToLower())
+                    ).ToList<Employee>();   
+                }
+                int totalRowsAfterFiltering = empList.Count;
+
+                //sorting
+                empList = empList.OrderBy(sortColName + " " + sortDir).ToList<Employee>();
+
+                //paging
+                empList = empList.Skip(start).Take(length).ToList<Employee>();
+
+                return Json(new {data = empList, draw = Request["draw"], recordsTotal = totalRows,
+                recordsFiltered = totalRowsAfterFiltering}, JsonRequestBehavior.AllowGet);
             }
         }
     }
